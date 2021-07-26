d830b4fa5608fa7d5082001850d37b3e5a2d46e4
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-07-25 21:34:04 -0600

    BaseRadio-BaseCheckbox

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
-
 const requireComponent = require.context(
   './components',
   false,
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -33,42 +33,36 @@
 
       <h3>Are pets allowed?</h3>
       <div>
-        <input
-            type="radio"
-            v-model="event.pets"
-            :value="1"
-            name="pets"
-          />
-        <label>Yes</label>
+        <BaseRadio
+          v-model="event.pets"
+          :value="1"
+          label="Yes"
+          name="pets"
+        />
       </div>
 
       <div>
-        <input
-          type="radio"
+        <BaseRadio
           v-model="event.pets"
           :value="0"
+          label="No"
           name="pets"
         />
-        <label>No</label>
       </div>
 
       <h3>Extras</h3>
       <div>
-        <input
-          type="checkbox"
+        <BaseCheckbox
           v-model="event.extras.catering"
-          class="field"
+          label="Catering"
         />
-        <label>Catering</label>
       </div>
 
       <div>
-        <input
-          type="checkbox"
+        <BaseCheckbox
           v-model="event.extras.music"
-          class="field"
+          label="Live music"
         />
-        <label>Live music</label>
       </div>
 
       <button class="button -fill-gradient" type="submit">Submit</button>
